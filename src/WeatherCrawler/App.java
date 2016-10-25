ff5c1f7c29b2c068e9fddf2c95c9aa0a670969b8
Hannes Berger <xaaris@googlemail.com>
2016-10-25 14:52:41 +0200

    alles nur geklaut

---
+++ src/WeatherCrawler/App.java
@@ -0,0 +1,37 @@
+/*
+ * Author: Andreas Loeffler
+ * E-Mail: andreas.loeffler@haw-hamburg.de 
+ */
+
+package WeatherCrawler;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Timer;
+import java.util.TimerTask;
+import org.json.JSONException;
+
+public class App extends TimerTask {
+	
+	public static void main(String[] args) {
+		
+		/*Create Timer. Time will execute every hour.*/
+		Timer timer = new Timer();
+		timer.schedule(new App(), 1000, 3600000);
+
+	}
+
+	@Override
+	public void run(){
+		WebCrawler crawler;
+		try {
+			crawler = new WebCrawler("http://api.citybik.es/v2/networks/stadtrad?fields=stations");
+			crawler.persistData();	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			MailNotification.sendMail(e);
+		} 
+			
+	}
+
+}
